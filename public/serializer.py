77ead260a2541d34689c7cc6d061759ffb96e6a9
Jaspreet-singh-1032 <jassimehra2612@gmail.com>
2022-09-04 04:42:01 +0530

    update available status

--- public/serializer.py
+++ public/serializer.py
@@ -1,8 +1,11 @@
 from rest_framework.serializers import (
     ModelSerializer,
+    ValidationError
 )
 
-
+from schedule.models import (
+    Slots
+)
 from .models import (
     Meeting,
 )
@@ -11,3 +14,17 @@ class MeetingSerializer(ModelSerializer):
     class Meta:
         model = Meeting
         fields = '__all__'
+    
+    def validate(self, attrs):
+        super().validate(attrs)
+        slot = attrs.get('slot')
+        meetings = slot.meetings.all()
+        print('=============================')
+        print(slot)
+        print(slot.max_people)
+        print(meetings.count())
+        if meetings.count() >= slot.max_people:
+            slot.is_available = False
+            slot.save()
+            raise ValidationError('No more participants can join.')
+        return attrs
